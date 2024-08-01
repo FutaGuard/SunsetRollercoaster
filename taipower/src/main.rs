3641caaed1759d23d793bc4d63371a7db3186306
tdc <tdc@tdccc.com.tw>
2024-08-01 17:19:25 +0800

    精簡過程

--- taipower/src/main.rs
+++ taipower/src/main.rs
@@ -1,5 +1,6 @@
 use regex::Regex;
 use reqwest;
+use std::collections::HashMap;
 use serde::{Deserialize};
 use serde_json::Value;
 use reqwest::header::{HeaderMap, HeaderValue, USER_AGENT};
@@ -10,8 +11,6 @@ struct TaiPowerData {
     #[serde(rename = "aaData")]
     aa_data: Vec<TaiPower>,
 }
-
-// AaDataItem {  description: "抽蓄水力(Pumped Hydro)", plant_name: "大觀二#1", capacity: "-", generation: "0.0", percentage: "-", status: " ", notes: "" }
 
 #[derive(Debug, Deserialize)]
 struct TaiPower {
@@ -30,13 +29,18 @@ fn remove_html_tags(input: &str) -> String {
     re.replace_all(input, "").to_string()
 }
 
-fn process_value(value: &Value) -> Value {
+fn process_value(value: &Value, leadzero: bool) -> Value {
     match value {
         Value::String(s) => {
             // 清理 HTML 標籤，並處理空字符串
-            if s.trim() == "-" {
+            if ["-", " "].contains(&s.as_str()) {
                 Value::String("".to_string())
-            } else {
+            } else if leadzero {
+                // 如果需要處理前導零，則對字符串進行處理
+                // Value::String()
+                Value::String("0".to_string())
+            }
+            else {
                 Value::String(remove_html_tags(s))
             }
         }
@@ -51,7 +55,7 @@ fn process_value(value: &Value) -> Value {
     }
 }
 
-async fn fetch() -> String {
+async fn fetch() -> HashMap<String, Value> {
     const URL: &str = "https://www.taipower.com.tw/d006/loadGraph/loadGraph/data/genary.json";
     let client = reqwest::Client::new();
     let ua = "*";
@@ -63,7 +67,7 @@ async fn fetch() -> String {
         .send()
         .await {
         Ok(response) => {
-            match response.text().await {
+            match response.json::<HashMap<String, Value>>().await {
                 Ok(data) => {
                     data
                 }
@@ -82,21 +86,24 @@ async fn fetch() -> String {
 
 #[tokio::main]
 async fn main() {
-    // println!("Hello, world!");
     env_logger::init();
-    let data = fetch().await;
-    // let v: Value = serde_json::from_str(&data).unwrap();
-    let mut v: Value = serde_json::from_str(&data).unwrap();
-    if let Some(aa_data) = v.get_mut("aaData").and_then(|d| d.as_array_mut()) {
-        for item in aa_data.iter_mut() {
-            if let Some(array) = item.as_array_mut() {
-                for i in 0..array.len() {
-                    if let Some(value) = array.get_mut(i) {
-                        *value = process_value(value);
+    let mut data = fetch().await;
+    data.get_mut("aaData")
+        .and_then(|v| v.as_array_mut())
+        .map(|f| {
+            for item in f.iter_mut() {
+                item.as_array_mut().map(|a| {
+                    for i in 0..a.len() {
+                        let mut leadzero = false;
+                        if [3, 4].contains(&i) {
+                            leadzero = true;
+                        }
+                        a[i] = process_value(&a[i], leadzero);
                     }
-                }
+                });
             }
-        }
-    }
-    let taipower: TaiPowerData = serde_json::from_value(v).unwrap();
+        });
+    
+    let taipower: TaiPowerData = serde_json::from_value(Value::Object(data.into_iter().collect())).unwrap();
+    
 }
