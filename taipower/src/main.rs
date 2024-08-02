7cb7a5fd8b3daf70fec844210bb20a4eb85ea95d
tdc <tdc@tdccc.com.tw>
2024-08-02 17:28:23 +0800

    處理時區

--- taipower/src/main.rs
+++ taipower/src/main.rs
@@ -1,13 +1,19 @@
 use regex::Regex;
 use reqwest;
 use std::collections::HashMap;
-use serde::{Deserialize};
+use serde::{Deserialize, Deserializer};
 use serde_json::Value;
 use reqwest::header::{HeaderMap, HeaderValue, USER_AGENT};
 use std::process;
+use sea_orm::{Database, DatabaseConnection};
+use dotenv::dotenv;
+use std::env;
+use chrono::prelude::*;
 
 #[derive(Debug, Deserialize)]
 struct TaiPowerData {
+    #[serde(rename = "", deserialize_with = "parse_datetime")]
+    timestamp: DateTime<FixedOffset>,
     #[serde(rename = "aaData")]
     aa_data: Vec<TaiPower>,
 }
@@ -22,6 +28,19 @@ struct TaiPower {
     percentage: Option<String>,
     status: Option<String>,
     notes: Option<String>,
+}
+
+fn parse_datetime<'de, D>(deserializer: D) -> Result<DateTime<FixedOffset>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    const FORMAT: &str = "%Y-%m-%d %H:%M";
+    let s = String::deserialize(deserializer)?;
+    let naive_datetime = NaiveDateTime::parse_from_str(&s, FORMAT)
+        .map_err(serde::de::Error::custom)?;
+    let offset = FixedOffset::east_opt(8 * 3600)
+        .ok_or_else(|| serde::de::Error::custom("Invalid offset"))?;
+    Ok(naive_datetime.and_local_timezone(offset).unwrap())
 }
 
 fn remove_html_tags(input: &str) -> String {
@@ -84,6 +103,20 @@ async fn fetch() -> HashMap<String, Value> {
     }
 }
 
+async fn dbinit() -> DatabaseConnection {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    match Database::connect(database_url).await {
+        Ok(db) => {
+            db
+        }
+        Err(err) => {
+            println!("Database Connection Error: {}", err);
+            process::exit(1);
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() {
     env_logger::init();
@@ -105,5 +138,6 @@ async fn main() {
         });
     
     let taipower: TaiPowerData = serde_json::from_value(Value::Object(data.into_iter().collect())).unwrap();
+    let db = dbinit().await;
     
 }
