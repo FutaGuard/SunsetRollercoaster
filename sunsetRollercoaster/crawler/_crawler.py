24062bebb93ea2eac581ec9d692b4936833288a6
eric <g9823235@yuntech.edu.tw>
2025-11-27 11:58:55 +0800

    feat: enhance Crawler with SSL context and improved client closure

--- sunsetRollercoaster/crawler/_crawler.py
+++ sunsetRollercoaster/crawler/_crawler.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
-from httpx import AsyncClient
+from httpx import AsyncClient,AsyncHTTPTransport
 import ua_generator
 from enum import Enum
 from typing import Any
+import truststore
 
 class Proxy(Enum):
     TW = "http://127.0.0.1:1080"
@@ -15,8 +16,20 @@ class Crawler(ABC):
         self.proxy = proxy
         self.user_agent = ua_generator.generate()
         headers = {**self.user_agent.headers}
-        self.client = AsyncClient(headers=headers, proxies={"http://": self.proxy.value, "https://": self.proxy.value})
 
+        ctx = truststore.SSLContext()
+        self.client = AsyncClient(
+            headers=headers,
+            proxies={"http://": self.proxy.value, "https://": self.proxy.value},
+            timeout=30,
+            follow_redirects=True,
+            http2=True,
+            transport= AsyncHTTPTransport(retries=3, verify=ctx),
+        )
+
+    async def close(self) -> None:
+        if hasattr(self, 'client') and self.client:
+            await self.client.aclose()
     @abstractmethod
     async def query(self, url: str) -> Any:
         pass
