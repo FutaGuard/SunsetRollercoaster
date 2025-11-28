7ca1c67932657767c5f11e9696d65ee672538c85
eric <g9823235@yuntech.edu.tw>
2025-11-28 11:52:36 +0800

    feat: 增加下載檔案的非同步方法並改進用戶代理標頭設置

--- sunsetRollercoaster/crawler/_crawler.py
+++ sunsetRollercoaster/crawler/_crawler.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from httpx import AsyncClient,AsyncHTTPTransport
+import httpx
 import ua_generator
 from enum import Enum
 from typing import Any
@@ -16,24 +17,59 @@ class Crawler(ABC):
         self.proxy = proxy
         self.user_agent = ua_generator.generate()
         # todo 這邊不知道為什麼出錯
-        # headers = {**self.user_agent.headers}
-        headers=''
+        #headers = {**self.user_agent.headers}
+        user_agent_string = getattr(self.user_agent, 'text',
+                            getattr(self.user_agent, 'user_agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'))
+
+        headers = {
+            "User-Agent": user_agent_string,
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "Accept-Language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-User": "?1",
+            "Cache-Control": "max-age=0",
+        }
+
         ctx = truststore.SSLContext()
         self.client = AsyncClient(
             headers=headers,
-            # proxies={"http://": self.proxy.value, "https://": self.proxy.value},
             timeout=30,
             follow_redirects=True,
             http2=True,
             transport= AsyncHTTPTransport(retries=3, verify=ctx,proxy=self.proxy.value),
         )
-
     async def close(self) -> None:
         if hasattr(self, 'client') and self.client:
             await self.client.aclose()
     async def __aenter__(self):
         return self
-
+    async def download_file_stream_async(self,url: str, save_path: str, chunk_size: int = 8192):
+        result ={}
+        try:
+            async with httpx.AsyncClient() as client:
+                async with client.stream("GET", url) as response:
+                    response.raise_for_status()
+                    with open(save_path, "wb") as f:
+                        async for chunk in response.aiter_bytes(chunk_size=chunk_size):
+                            if chunk:
+                                f.write(chunk)
+            result = {
+                'success':True,
+                'reason':'檔案下載完成'
+            }
+        except Exception as e:
+            result={
+                'success':False,
+                'status_code':e.response.status_code,
+                'reason': '檔案下載失敗',
+                'text':e.response.text
+            }
+        return result
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         await self.close()
 
--- test.py
+++ test.py
@@ -1,11 +1,17 @@
-from sunsetRollercoaster.crawler.invoice import InvoiceCrawler
-from sunsetRollercoaster.crawler._crawler import Proxy  # 絕對導入
+from sunsetRollercoaster.crawler.invoice import InvoiceCrawler,Proxy
 import asyncio
 
 async def main():
     async with InvoiceCrawler(proxy=Proxy.NO) as crawler:
-        result = await crawler.query("https://httpbin.org/json")
-        print(result)
+        #result = await crawler.query("https://httpbin.org/json")
+        #print(result)
+        ret = await crawler.client.get("https://www.solarbus.com.tw")
+        print(ret.text)
+
+        ret = await crawler.download_file_stream_async("http://http.speed.hinet.net/test_020m11.zip","test.zip")
+        print(ret)
+
+    await crawler.close()
 
 if __name__ == "__main__":
     asyncio.run(main())
