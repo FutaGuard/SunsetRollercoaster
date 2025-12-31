9deccd187a3f153c0305fb46ea05ad59c08a1b0b
eric <g9823235@yuntech.edu.tw>
2025-12-31 14:39:32 +0800

    feat: 更新 check_car 方法的參數名稱以提高可讀性

--- test.py
+++ test.py
@@ -4,7 +4,8 @@ from sunsetRollercoaster.component.mvdis import car_check
 
 async def main():
     # r = await car_check.CarCheck().client.get('https://www.solarbus.com.tw')
-    resp = await car_check.CarCheck().check_car('2',companyNo='96976746',plateNo='AVH-8180')
+    resp = await car_check.CarCheck().check_car(queryType='2',companyNo='96976746',plateNo='AVH-8180')
+    resp = await car_check.CarCheck().check_car(queryType='1',birthday='0670319',idNo='Q122548647')
     print(resp)
 
 
